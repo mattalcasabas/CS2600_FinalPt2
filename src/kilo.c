ed09d2c41e3727736a54b6e3001a55ec058f8741
mattalcasabas <74041010+mattalcasabas@users.noreply.github.com>
2021-12-04 15:28:31 -0800

    chapter 3: move the cursor

--- src/kilo.c
+++ src/kilo.c
@@ -18,6 +18,7 @@
 
 struct editorConfig
 {
+   int cx, cy;
    int screenrows;
    int screencols;
    struct termios orig_termios;
@@ -186,7 +187,11 @@ void editorRefreshScreen()
    abAppend(&ab, "\x1b[?25l", 6);
    abAppend(&ab, "\x1b[H", 3);
    editorDrawRows(&ab);
-   abAppend(&ab, "\x1b[H", 3);
+
+   char buf[32];
+   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+   abAppend(&ab, buf, strlen(buf));
+
    abAppend(&ab, "\x1b[?25h", 6);
 
    write(STDOUT_FILENO, ab.b, ab.len);
@@ -194,6 +199,25 @@ void editorRefreshScreen()
 }
 
 /*** input ***/
+
+void editorMoveCursor(char key)
+{
+   switch (key)
+   {
+   case 'a':
+      E.cx--;
+      break;
+   case 'd':
+      E.cx++;
+      break;
+   case 'w':
+      E.cy--;
+      break;
+   case 's':
+      E.cy++;
+      break;
+   }
+}
 
 void editorProcessKeypress()
 {
@@ -206,6 +230,13 @@ void editorProcessKeypress()
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
+
+   case 'w':
+   case 's':
+   case 'a':
+   case 'd':
+      editorMoveCursor(c);
+      break;
    }
 }
 
@@ -213,6 +244,9 @@ void editorProcessKeypress()
 
 void initEditor()
 {
+   E.cx = 0;
+   E.cy = 0;
+
    if (getWindowSize(&E.screenrows, &E.screencols) == -1)
       die("getWindowSize");
 }
