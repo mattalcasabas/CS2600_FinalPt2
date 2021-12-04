3e001c03814966af1c9ae3300b9d38678c30ee50
mattalcasabas <74041010+mattalcasabas@users.noreply.github.com>
2021-12-04 15:51:27 -0800

    chapter 3: the delete key

--- src/kilo.c
+++ src/kilo.c
@@ -20,6 +20,7 @@ enum editorKey
    ARROW_RIGHT,
    ARROW_UP,
    ARROW_DOWN,
+   DEL_KEY,
    HOME_KEY,
    END_KEY,
    PAGE_UP,
@@ -102,6 +103,8 @@ int editorReadKey()
                {
                case '1':
                   return HOME_KEY;
+               case '3':
+                  return DEL_KEY;
                case '4':
                   return END_KEY;
                case '5':
