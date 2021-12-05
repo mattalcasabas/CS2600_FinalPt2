80f74ea744addea5301526d88e396e5735c46970
mattalcasabas <74041010+mattalcasabas@users.noreply.github.com>
2021-12-05 15:51:24 -0800

    chapter 4: vertical scrolling

--- src/kilo.c
+++ src/kilo.c
@@ -43,6 +43,7 @@ typedef struct erow
 struct editorConfig
 {
    int cx, cy;
+   int rowoff;
    int screenrows;
    int screencols;
    int numrows;
@@ -276,12 +277,25 @@ void abFree(struct abuf *ab)
 
 /*** output ***/
 
+void editorScroll()
+{
+   if (E.cy < E.rowoff)
+   {
+      E.rowoff = E.cy;
+   }
+   if (E.cy >= E.rowoff + E.screenrows)
+   {
+      E.rowoff = E.cy - E.screenrows + 1;
+   }
+}
+
 void editorDrawRows(struct abuf *ab)
 {
    int y;
    for (y = 0; y < E.screenrows; y++)
    {
-      if (y >= E.numrows)
+      int filerow = y + E.rowoff;
+      if (filerow >= E.numrows)
       {
          if (E.numrows == 0 && y == E.screenrows / 3)
          {
@@ -313,16 +327,17 @@ void editorDrawRows(struct abuf *ab)
       }
       else
       {
-         int len = E.row[y].size;
+         int len = E.row[filerow].size;
          if (len > E.screencols)
             len = E.screencols;
-         abAppend(ab, E.row[y].chars, len);
+         abAppend(ab, E.row[filerow].chars, len);
       }
    }
 }
 
 void editorRefreshScreen()
 {
+   editorScroll();
    struct abuf ab = ABUF_INIT;
 
    abAppend(&ab, "\x1b[?25l", 6);
@@ -330,7 +345,7 @@ void editorRefreshScreen()
    editorDrawRows(&ab);
 
    char buf[32];
-   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
    abAppend(&ab, buf, strlen(buf));
 
    abAppend(&ab, "\x1b[?25h", 6);
@@ -364,7 +379,7 @@ void editorMoveCursor(int key)
       }
       break;
    case ARROW_DOWN:
-      if (E.cy != E.screenrows - 1)
+      if (E.cy < E.numrows)
       {
          E.cy++;
       }
@@ -415,6 +430,7 @@ void initEditor()
 {
    E.cx = 0;
    E.cy = 0;
+   E.rowoff = 0;
    E.numrows = 0;
    E.row = NULL;
 
