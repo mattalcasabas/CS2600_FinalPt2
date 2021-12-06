bb2a88ac284c293ef8bac1b692e6e7991a2d9b75
mattalcasabas <74041010+mattalcasabas@users.noreply.github.com>
2021-12-05 16:46:04 -0800

    ch 4: tabs and the cursor

--- src/kilo.c
+++ src/kilo.c
@@ -46,6 +46,7 @@ typedef struct erow
 struct editorConfig
 {
    int cx, cy;
+   int rx;
    int rowoff;
    int coloff;
    int screenrows;
@@ -314,6 +315,8 @@ void abFree(struct abuf *ab)
 
 void editorScroll()
 {
+   E.rx = E.cx;
+
    if (E.cy < E.rowoff)
    {
       E.rowoff = E.cy;
@@ -322,13 +325,13 @@ void editorScroll()
    {
       E.rowoff = E.cy - E.screenrows + 1;
    }
-   if (E.cx < E.coloff)
+   if (E.rx < E.coloff)
    {
-      E.coloff = E.cx;
+      E.coloff = E.rx;
    }
-   if (E.cx >= E.coloff + E.screencols)
+   if (E.rx >= E.coloff + E.screencols)
    {
-      E.coloff = E.cx - E.screencols + 1;
+      E.coloff = E.rx - E.screencols + 1;
    }
 }
 
@@ -391,7 +394,7 @@ void editorRefreshScreen()
 
    char buf[32];
    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
-            (E.cx - E.coloff) + 1);
+            (E.rx - E.coloff) + 1);
    abAppend(&ab, buf, strlen(buf));
 
    abAppend(&ab, "\x1b[?25h", 6);
@@ -494,6 +497,7 @@ void initEditor()
 {
    E.cx = 0;
    E.cy = 0;
+   E.rx = 0;
    E.rowoff = 0;
    E.coloff = 0;
    E.numrows = 0;
