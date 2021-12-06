bc32b31249badff9b43cd1f87e6b5854471969c3
mattalcasabas <74041010+mattalcasabas@users.noreply.github.com>
2021-12-06 13:35:24 -0800

    ch 6: hightlight search

--- src/kilo.c
+++ src/kilo.c
@@ -41,7 +41,8 @@ enum editorKey
 enum editorHighlight
 {
    HL_NORMAL = 0,
-   HL_NUMBER
+   HL_NUMBER,
+   HL_MATCH
 };
 
 /*** data ***/
@@ -261,6 +262,8 @@ int editorSyntaxToColor(int hl)
    {
    case HL_NUMBER:
       return 31;
+   case HL_MATCH:
+      return 34;
    default:
       return 37;
    }
@@ -569,6 +572,7 @@ void editorFindCallback(char *query, int key)
          E.cy = current;
          E.cx = editorRowRxToCx(row, match - row->render);
          E.rowoff = E.numrows;
+         memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
          break;
       }
    }
