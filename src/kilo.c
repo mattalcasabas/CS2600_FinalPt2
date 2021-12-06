c94832a72458b3fb969b6f8d97833170cf2a9078
mattalcasabas <74041010+mattalcasabas@users.noreply.github.com>
2021-12-05 19:00:12 -0800

    ch 5: the enter key

--- src/kilo.c
+++ src/kilo.c
@@ -272,9 +272,12 @@ void editorUpdateRow(erow *row)
    row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len)
+void editorInsertRow(int at, char *s, size_t len)
 {
+   if (at < 0 || at > E.numrows)
+      return;
    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+   memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
    int at = E.numrows;
    E.row[at].size = len;
    E.row[at].chars = malloc(len + 1);
@@ -283,7 +286,6 @@ void editorAppendRow(char *s, size_t len)
    E.row[at].rsize = 0;
    E.row[at].render = NULL;
    editorUpdateRow(&E.row[at]);
-
    E.numrows++;
    E.dirty++;
 }
@@ -341,10 +343,29 @@ void editorInsertChar(int c)
 {
    if (E.cy == E.numrows)
    {
-      editorAppendRow("", 0);
+      editorInsertRow(E.numrows, "", 0);
    }
    editorRowInsertChar(&E.row[E.cy], E.cx, c);
    E.cx++;
+}
+
+void editorInsertNewline()
+{
+   if (E.cx == 0)
+   {
+      editorInsertRow(E.cy, "", 0);
+   }
+   else
+   {
+      erow *row = &E.row[E.cy];
+      editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+      row = &E.row[E.cy];
+      row->size = E.cx;
+      row->chars[row->size] = '\0';
+      editorUpdateRow(row);
+   }
+   E.cy++;
+   E.cx = 0;
 }
 
 void editorDelChar()
@@ -407,7 +428,7 @@ void editorOpen(char *filename)
       while (linelen > 0 && (line[linelen - 1] == '\n' ||
                              line[linelen - 1] == '\r'))
          linelen--;
-      editorAppendRow(line, linelen);
+      editorInsertRow(E.numrows, line, linelen);
    }
    free(line);
    fclose(fp);
@@ -675,7 +696,7 @@ void editorProcessKeypress()
    switch (c)
    {
    case '\r':
-      /* TODO */
+      editorInsertNewline();
       break;
    case CTRL_KEY('q'):
       if (E.dirty && quit_times > 0)
